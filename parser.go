a56769afb5d8c9f0da042d97debb3057724c9fe1
Dmitry Pokidov <dmitry.pokidov@medibank.com.au>
2015-10-08 10:26:37 +1100

    Initial commit. Moved from bitbucket.

---
+++ parser.go
@@ -0,0 +1,56 @@
+package gounexport
+import (
+	"github.com/dooman87/gounexport/util"
+	"github.com/dooman87/gounexport/fs"
+	"go/token"
+	"go/types"
+	"github.com/dooman87/gounexport/importer"
+)
+
+//ParsePackage parses package and filling info structure.
+//It's will fill info about all internal packages if they
+//are not used in the root package.
+func ParsePackage(pkgName string, info *types.Info) (*types.Package, *token.FileSet, error) {
+	collectImporter := new(importer.CollectInfoImporter)
+	collectImporter.Info = info
+
+	var resultPkg *types.Package
+	var resultFset *token.FileSet
+	parsedPackages := make(map[string]bool)
+
+	notParsedPackage := pkgName
+	for len(notParsedPackage) > 0 {
+		collectImporter.Pkg = notParsedPackage
+		pkg, fset, err := collectImporter.Collect()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		//Filling results only from first package
+		//that was passed as argument to function
+		if resultPkg == nil {
+			resultPkg = pkg
+			resultFset = fset
+		}
+		parsedPackages[notParsedPackage] = true
+
+		//Searching for a new package that was not parsed before
+		notParsedPackage = ""
+		files, err := fs.GetUnusedSources(pkgName, fset)
+		if err != nil {
+			return nil, nil, err
+		}
+		for _, f := range files {
+			newNotParsedPackage := fs.GetPackagePath(f)
+			if !parsedPackages[newNotParsedPackage] {
+				notParsedPackage = newNotParsedPackage
+				break
+			} else {
+				util.Info("package %s has been already parsed, however %s file is still unused", newNotParsedPackage, f)
+			}
+		}
+	}
+
+	return resultPkg, resultFset, nil
+}
+
